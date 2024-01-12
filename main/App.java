77e7d2ed59350081131e02a510d48465c007e1c7
AlbertGuru <156181009+AlbertGuru@users.noreply.github.com>
2024-01-12 09:59:47 +0200

    Create App.java

---
+++ main/App.java
@@ -0,0 +1,11 @@
+// Ensin luokan määrittely
+public class App {
+    /* Sitten main-operaatio eli pääohjelma,
+     * joka määritetään aina samalla tavalla.
+     * Tähän pääoperaatioon siis kuuluu otsikko ja runko */
+    public static void main(String args[]) {
+        // Tulostus
+        System.out.println("Hello World!");
+    }    
+}
+// ohjelma toimii mikä on aika kiva
